a05a122d0523c0cd896343035af205e18421da18
Nilay Arangil <nilayarangil@gmail.com>
2025-02-16 14:42:13 -0600

    Shows how to extract text from images

---
+++ ExtractTextFromNotes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+
+namespace ComputerVisionTest
+{
+    
+    public class ExtractTextFromNotes
+    {        
+        public static async Task ReadFileUrl(string urlFile, string Key, string endPoint)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("READ FILE FROM URL");
+            Console.WriteLine();
+
+            string key = Key;
+            string endpoint = endPoint;
+
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+
+            // Read text from URL
+            var textHeaders = await client.ReadAsync(urlFile);
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+            Thread.Sleep(2000);
+
+            // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            ReadOperationResult results;
+            Console.WriteLine($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+            Console.WriteLine();
+            do
+            {
+                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running ||
+                results.Status == OperationStatusCodes.NotStarted));
+
+            // Display the found text.
+            Console.WriteLine();
+            var textUrlFileResults = results.AnalyzeResult.ReadResults;
+            foreach (ReadResult page in textUrlFileResults)
+            {
+                foreach (Line line in page.Lines)
+                {
+                    Console.WriteLine(line.Text);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ComputerVisionTest;
+﻿using ComputerVisionTest;
 
 string subkey = "yourkey";
 string endpnt = "https://nilayazurecompvisioninstance.cognitiveservices.azure.com/";
 ImageAnalyser IA = new ImageAnalyser(subkey, endpnt);
 
-// Let the user pick which file to analyze
+// Pass an Image to analyse.
 string? imagePath = @"C:\Projects\ComputerVisionTest\images\nilaytest.jpg";// ImageSelectionHelper.SelectImage(Directory.GetFiles("images"));
+string? read_txt_from_image = "https://raw.githubusercontent.com/nilayarangil/AzureComputerVisionTests/refs/heads/master/images/notes1.jpg";
 
 // The user is allowed to not pick an image, in which case we just exit
 if (string.IsNullOrWhiteSpace(imagePath)) return;
@@ -14,7 +14,5 @@ if (string.IsNullOrWhiteSpace(imagePath)) return;
 // Have Computer Vision analyze the image            
 List<string> detectedItems = IA.DetectItemsAsync(imagePath).Result;
 
-IEnumerable<string> itemsToMention = detectedItems.Distinct().Take(5);
-string message = $"Summary: {string.Join(", ", itemsToMention)}";
-Console.WriteLine(message);
-
+//Extract text from Image
+ComputerVisionTest.ExtractTextFromNotes.ReadFileUrl(read_txt_from_image, subkey, endpnt).Wait();
