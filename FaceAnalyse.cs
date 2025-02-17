ee53f65be36ee1793fbda2de7460b7c635d42121
Nilay Arangil <nilayarangil@gmail.com>
2025-02-16 18:32:47 -0600

    Added Face Analysis code for detecting gender and age from an image.

---
+++ FaceAnalyse.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ComputerVisionTest
+{
+
+
+    public class FaceAnalyse
+    {
+        private static readonly FaceAttributeType[] faceAttributes =
+          { FaceAttributeType.Age, FaceAttributeType.Gender };
+
+        public static string AnalyseFace(string imageURL, string Key, string endPoint)
+        {
+            var faceClient = new FaceClient(new ApiKeyServiceClientCredentials(Key));
+            faceClient.Endpoint = endPoint;
+
+            Console.WriteLine("Faces being detected ...");
+            var t1 = DetectRemoteAsync(faceClient, imageURL);
+
+            Task.WhenAll(t1).Wait(5000);
+
+            return "Press any key to exit";
+          
+        }
+
+        // Detect faces in a remote image
+        private static async Task DetectRemoteAsync(FaceClient faceClient, string imageUrl)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine("\nInvalid remoteImageUrl:\n{0} \n", imageUrl);
+                return;
+            }
+
+            try
+            {
+                IList<DetectedFace> faceList =
+                    await faceClient.Face.DetectWithUrlAsync(
+                        imageUrl, true, false, faceAttributes);
+
+                DisplayAttributes(GetFaceAttributes(faceList, imageUrl), imageUrl);
+            }
+            catch (APIErrorException e)
+            {
+                Console.WriteLine(imageUrl + ": " + e.Message);
+            }
+        }
+
+        private static string GetFaceAttributes(
+           IList<DetectedFace> faceList, string imagePath)
+        {
+            string attributes = string.Empty;
+
+            foreach (DetectedFace face in faceList)
+            {
+                double? age = face.FaceAttributes.Age;
+                string gender = face.FaceAttributes.Gender.ToString();
+                attributes += gender + " " + age + "   ";
+            }
+
+            return attributes;
+        }
+
+        // Display the face attributes
+        private static void DisplayAttributes(string attributes, string imageUri)
+        {
+            Console.WriteLine(imageUri);
+            Console.WriteLine(attributes + "\n");
+        }
+
+    }
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,11 @@ string subkey = "yourkey";
 string endpnt = "https://nilayazurecompvisioninstance.cognitiveservices.azure.com/";
 ImageAnalyser IA = new ImageAnalyser(subkey, endpnt);
 
-// Pass an Image to analyse.
+// Path to a local image to analyse.
 string? imagePath = @"C:\Projects\ComputerVisionTest\images\nilaytest.jpg";// ImageSelectionHelper.SelectImage(Directory.GetFiles("images"));
+// Path to a remote image to analyse.
+string? imageURLPath = "https://raw.githubusercontent.com/nilayarangil/AzureComputerVisionTests/refs/heads/master/images/nilaytest.jpg";
+// Path to a remote Image (note) to analyse.
 string? read_txt_from_image = "https://raw.githubusercontent.com/nilayarangil/AzureComputerVisionTests/refs/heads/master/images/notes1.jpg";
 
 // The user is allowed to not pick an image, in which case we just exit
@@ -16,3 +19,6 @@ List<string> detectedItems = IA.DetectItemsAsync(imagePath).Result;
 
 //Extract text from Image
 ComputerVisionTest.ExtractTextFromNotes.ReadFileUrl(read_txt_from_image, subkey, endpnt).Wait();
+
+//Extract Attributes of a face. Gender and Age.
+ComputerVisionTest.FaceAnalyse.AnalyseFace(imageURLPath, subkey, endpnt);
